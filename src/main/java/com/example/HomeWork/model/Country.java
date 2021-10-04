f89db97ec9383a581bb2f9062583d0f97c4db760
0ndrei <lungu.andrei.00@mail.ru>
2021-10-04 19:17:32 +0300

    Create table for DB and add method CRUD in CountryService

--- src/main/java/com/example/HomeWork/model/Country.java
+++ src/main/java/com/example/HomeWork/model/Country.java
@@ -1,4 +1,22 @@
 package com.example.HomeWork.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "Countries")
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "code")
+    private String code;
+
 }
--- src/main/java/com/example/HomeWork/service/CountryService.java
+++ src/main/java/com/example/HomeWork/service/CountryService.java
@@ -1,4 +1,13 @@
 package com.example.HomeWork.service;
 
+import com.example.HomeWork.model.Country;
+
+import java.util.List;
+
 public interface CountryService {
+    Country save (Country country);
+    List<Country> getAll();
+    Country get(long id);
+    Country updateCountry(Country country, long id);
+    void delete(long id);
 }
