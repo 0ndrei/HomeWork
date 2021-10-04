b639aac0ba034efc112eae89d3aa79e6b9426ff9
0ndrei <lungu.andrei.00@mail.ru>
2021-10-04 20:42:04 +0300

    Refactor java class to interface and add extends JpaRepository

--- src/main/java/com/example/HomeWork/repository/CityRepository.java
+++ src/main/java/com/example/HomeWork/repository/CityRepository.java
@@ -1,4 +1,7 @@
 package com.example.HomeWork.repository;
 
-public class CityRepository {
+import com.example.HomeWork.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
 }
