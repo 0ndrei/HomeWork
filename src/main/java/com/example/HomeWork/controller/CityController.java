691c0a0fa7da6b0d7acfac2aa2efd896cf4668d4
0ndrei <lungu.andrei.00@mail.ru>
2021-10-04 20:44:29 +0300

    Add method create for City

--- src/main/java/com/example/HomeWork/controller/CityController.java
+++ src/main/java/com/example/HomeWork/controller/CityController.java
@@ -1,4 +1,26 @@
 package com.example.HomeWork.controller;
 
+import com.example.HomeWork.model.City;
+import com.example.HomeWork.service.CityService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cities")
 public class CityController {
+    private CityService cityService;
+
+    public CityController(CityService cityService){
+        super();
+        this.cityService = cityService;
+    }
+
+    @PostMapping
+    public ResponseEntity<City> create(@RequestBody City city){
+        return new ResponseEntity<City>(cityService.save(city), HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/example/HomeWork/service/impl/CityServiceImpl.java
+++ src/main/java/com/example/HomeWork/service/impl/CityServiceImpl.java
@@ -1,4 +1,41 @@
 package com.example.HomeWork.service.impl;
 
-public class CityServiceImpl {
+import com.example.HomeWork.model.City;
+import com.example.HomeWork.repository.CityRepository;
+import com.example.HomeWork.service.CityService;
+
+import java.util.List;
+
+public class CityServiceImpl implements CityService {
+    CityRepository cityRepository;
+
+    public CityServiceImpl(CityRepository cityRepository){
+        super();
+        this.cityRepository = cityRepository;
+    }
+
+    @Override
+    public City save(City city) {
+        return cityRepository.save(city);
+    }
+
+    @Override
+    public List<City> getAll() {
+        return null;
+    }
+
+    @Override
+    public City get(long id) {
+        return null;
+    }
+
+    @Override
+    public City update(City City, long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
 }
