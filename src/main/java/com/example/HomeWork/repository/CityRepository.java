6b229532014eaeb21fd4a32d1ca9c7c3376d9e85
0ndrei <lungu.andrei.00@mail.ru>
2021-10-05 14:07:03 +0300

    Create new xml file

--- src/main/java/com/example/HomeWork/repository/CityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.HomeWork.repository;
-
-import com.example.HomeWork.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityRepository extends JpaRepository<City, Long> {
-}
