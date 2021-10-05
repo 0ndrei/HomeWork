c29ecd431033a658e11ec37c7ef2ec1e9c772438
0ndrei <lungu.andrei.00@mail.ru>
2021-10-05 14:07:24 +0300

    Create new xml file

---
+++ src/main/java/com/example/HomeWork/service/impl/CityServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.HomeWork.service.impl;
+
+import com.example.HomeWork.exception.ResourceNotFoundException;
+import com.example.HomeWork.model.City;
+import com.example.HomeWork.repository.CityRepository;
+import com.example.HomeWork.service.CityService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityServiceImpl implements CityService {
+    CityRepository cityRepository;
+
+    public CityServiceImpl(CityRepository cityRepository){
+        super();
+        this.cityRepository = cityRepository;
+    }
+
+    @Override
+    public City save(City city) {
+        return cityRepository.save(city);
+    }
+
+    @Override
+    public List<City> getAll() {
+        return cityRepository.findAll();
+    }
+
+    @Override
+    public City get(long id) {
+        return cityRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("Country", "id", id));
+    }
+    @Override
+    public City update(City city, long id) {
+        City existingCity = cityRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("City", "id", id));
+        existingCity.setName(city.getName());
+        cityRepository.save(existingCity);
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+        cityRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("City", "id", id));
+        cityRepository.deleteById(id);
+    }
+}
